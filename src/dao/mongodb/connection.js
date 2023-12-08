9e4ebe1cd30d59670fca10c9d02b87c4cfaa6dcc
MantraGitH <camilo.arias56@hotmail.com>
2023-12-07 21:38:43 -0300

    commit1

---
+++ src/dao/mongodb/connection.js
@@ -0,0 +1,12 @@
+import { connect } from "mongoose";
+
+export const MONGO_URL =
+  "mongodb+srv://camiloarias56:camiloarias56@clusterprdm.as8xfkk.mongodb.net/ecommerce?retryWrites=true&w=majority";
+export const initMongoDB = async () => {
+  try {
+    await connect(MONGO_URL);
+    console.log("Conectado a MongoDB");
+  } catch {
+    console.log("Error al conectar con MongoDB");
+  }
+};
